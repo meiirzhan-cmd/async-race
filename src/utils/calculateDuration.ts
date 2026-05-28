0f17f314cd7004c3c1b8951692941c64aa50fb0e
Meiirzhan Baitangatov <rikimarubro@gmail.com>
2026-05-28 19:49:55 +0500

    docs: add explanatory comment to calculateDuration

--- src/utils/calculateDuration.ts
+++ src/utils/calculateDuration.ts
@@ -1,5 +1,6 @@
 import { MS_PER_SECOND } from '../constants/race';
 
+// Converts API engine response (distance/velocity in ms) to seconds for CSS transitions.
 export function calculateDuration(distance: number, velocity: number): number {
   return distance / velocity / MS_PER_SECOND;
 }
